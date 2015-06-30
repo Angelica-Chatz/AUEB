f5e2a678978e44c843d5e1851be5aa3a356d0040
StasinosKarampatsos <StasinosKarampatsos@users.noreply.github.com>
2015-07-01 02:30:21 +0300

    Update server.R

--- server.R
+++ server.R
@@ -1,26 +1,21 @@
 library(shiny)
+
+data<-read.csv("tdatafile.csv", header=TRUE)
+
 # Define a server for the Shiny app
 shinyServer(function(input, output) {
   
   # Fill in the spot we created for a plot
   output$museumPlot <- renderPlot({
     
-    # calculating barplot ylim
-    m<-tplotdata[13,input$Museum]
-    million<-as.logical(round(m/1000000))
-    hunthou<-as.logical(round(m/100000))
-    tenthou<-as.logical(round(m/10000))
-    thousan<-as.logical(round(m/1000))
-    hundred<-as.logical(round(m/100))
-    tendeka<-as.logical(round(m/10))
-    max<-m+million*1000000+hunthou*100000+tenthou*10000+thousan*1000+hundred*100+tendeka*10
     
     # Render a barplot
-    barplot(tplotdata[1:12,input$Museum], 
-            main=input$Month,
-            ylab="Number of Visitors",
-            xlab="Month",
+    x<-data[,input$Museum]
+    
+    barplot(x,
+            names=c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"),
             col="violetred",
-            ylim=c(0, max))
+            ylab="Number of Visitors",
+            xlab="Month")
   })
 })
